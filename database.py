4413460021796e8f34cf1fc3b8919685f1be3740
Atlas2234 <dlz988675@gmail.com>
2025-01-24 21:10:21 -0500

    added database connections, database tables, and fetching query data

--- database.py
+++ database.py
@@ -3,11 +3,226 @@ import psycopg2
 # Get the connection string of the cloud database
 connection_string = "postgresql://Weather_owner:1hRxBuzOP3vr@ep-weathered-boat-a887jtki.eastus2.azure.neon.tech/Weather?sslmode=require"
 
-def connect_to_database():
+
+
+def insert_to_database(raw_data):
     try:
         # Connect to the Neon database using the connection string
         conn = psycopg2.connect(connection_string)
         print("Connected to the database successfully")
 
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+
+        # Create command to insert weather data into the raw data table on the cloud database
+        for entry in raw_data:
+            # SQL query with %s placeholders for parameterized queries
+            query = "INSERT INTO raw_data(city, weather, temperature, feels_like, max_temp, min_temp, humidity, wind_speed) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+            
+            # Data to be inserted into the parameters of the SQL query
+            data = (entry["name"], entry["weather"], entry["temperature"], entry["feels"], entry["max_temp"], entry["min_temp"], entry["humidity"], entry["wind_speed"])
+            
+            # Execute the query with the data
+            cursor.execute(query, data)
+
+            # Commit the changes
+            conn.commit()
+        
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+
+
+def fetch_top_temp():
+    try:
+        # Connect to the Neon database using the connection string
+        conn = psycopg2.connect(connection_string)
+        print("Connected to the database successfully")
+
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        
+        # SQL query
+        select_query = "SELECT * FROM top_temperature;"
+        cursor.execute(select_query)
+
+        # Fetch rows from query result
+        rows = cursor.fetchall()
+        
+        # Commit the changes
+        conn.commit()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+        return rows
+    
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+
+
+def fetch_city_weather():
+    try:
+        # Connect to the Neon database using the connection string
+        conn = psycopg2.connect(connection_string)
+        print("Connected to the database successfully")
+
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        
+        # SQL query
+        select_query = "SELECT * FROM weather;"
+        cursor.execute(select_query)
+
+        # Fetch rows from query result
+        rows = cursor.fetchall()
+        
+        # Commit the changes
+        conn.commit()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+        return rows
+    
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+
+
+def fetch_max_temp():
+    try:
+        # Connect to the Neon database using the connection string
+        conn = psycopg2.connect(connection_string)
+        print("Connected to the database successfully")
+
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        
+        # SQL query
+        select_query = "SELECT * FROM top_max_temperature;"
+        cursor.execute(select_query)
+
+        # Fetch rows from query result
+        rows = cursor.fetchall()
+        
+        # Commit the changes
+        conn.commit()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+        return rows
+    
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+
+
+def fetch_min_temp():
+    try:
+        # Connect to the Neon database using the connection string
+        conn = psycopg2.connect(connection_string)
+        print("Connected to the database successfully")
+
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        
+        # SQL query
+        select_query = "SELECT * FROM low_min_temperature;"
+        cursor.execute(select_query)
+
+        # Fetch rows from query result
+        rows = cursor.fetchall()
+        
+        # Commit the changes
+        conn.commit()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+        return rows
+    
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+
+
+def fetch_humidity():
+    try:
+        # Connect to the Neon database using the connection string
+        conn = psycopg2.connect(connection_string)
+        print("Connected to the database successfully")
+
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        
+        # SQL query
+        select_query = "SELECT * FROM humidity;"
+        cursor.execute(select_query)
+
+        # Fetch rows from query result
+        rows = cursor.fetchall()
+        
+        # Commit the changes
+        conn.commit()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+        return rows
+    
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+
+
+def fetch_top_wind_speeds():
+    try:
+        # Connect to the Neon database using the connection string
+        conn = psycopg2.connect(connection_string)
+        print("Connected to the database successfully")
+
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
+        
+        # SQL query
+        select_query = "SELECT * FROM top_wind_speeds;"
+        cursor.execute(select_query)
+
+        # Fetch rows from query result
+        rows = cursor.fetchall()
+        
+        # Commit the changes
+        conn.commit()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        for row in rows:
+            print(row)
+
+        return rows
+    
     except Exception as e:
         print(f"Error connecting to the database: {e}")
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # Import the get_city_weather function from the weather_api_calls module
 from weather_api_calls import get_city_weather, organize_weather_data
 
-from database import connect_to_database
+from database import insert_to_database, fetch_top_temp, fetch_city_weather, fetch_max_temp, fetch_min_temp, fetch_humidity, fetch_top_wind_speeds
 
 if __name__ == "__main__":
     cities = get_city_weather()
     dicts = organize_weather_data(cities)
-    print(dicts)
+    #print(dicts)
 
-    connect_to_database()
+    #insert_to_database(dicts)
+    top_temp_data = fetch_top_temp()
+    weather_data = fetch_city_weather()
+    max_temp_data = fetch_max_temp()
+    min_temp_data = fetch_min_temp()
+    humidity_data = fetch_humidity()
+    wind_speed_data = fetch_top_wind_speeds()
